3f9279a5b24ef60c2bf398cc4dadcdfdf321dc5e
MilaDog <daniel.ryan.sergeant@gmail.com>
2022-12-03 17:55:05 +0200

    Update: Day 3 2022 [py]

--- 2022/03/03.py
+++ 2022/03/03.py
@@ -1,6 +1,3 @@
-from timeit import timeit
-
-
 def calc_score(x: chr) -> int:
     if 'a' <= x <= 'z':
         return ord(x) - ord('a') + 1
@@ -8,27 +5,26 @@ def calc_score(x: chr) -> int:
         return ord(x) - ord('A') + 27
 
 
-def solve():
-    backpacks = open("input.txt").readlines()
+def solve() -> None:
+    backpacks = open("input.txt").read().strip().split("\n")
 
+    # Part 1
     tlt1: int = 0
-    tlt2: int = 0
     for backpack in backpacks:
-        backpack.strip()
-        b1, b2 = backpack[:len(backpack) // 2], backpack[len(backpack) // 2:]
+        middle: int = len(backpack) // 2
+        b1, b2 = backpack[:middle], backpack[middle:]
 
-        for x in b1:
-            if x in b2:
-                tlt1 += calc_score(x)
-                break
-
-    for i in range(0, len(backpacks), 3):
-        for x in backpacks[i].strip():
-            if x in backpacks[i + 1] and x in backpacks[i + 2]:
-                tlt2 += calc_score(x)
-                break
+        common_char = next(iter(set(b1) & set(b2)))
+        tlt1 += calc_score(common_char)
 
     print(f"Part 1: {tlt1}")
+
+    # Part 2
+    tlt2: int = 0
+    for i in range(0, len(backpacks), 3):
+        common_char = next(iter(set(backpacks[i]) & set(backpacks[i + 1]) & set(backpacks[i + 2])))
+        tlt2 += calc_score(common_char)
+
     print(f"Part 2: {tlt2}")
 
 
---
+++ 2022/03/03_old.py
@@ -0,0 +1,32 @@
+def calc_score(x: chr) -> int:
+    if 'a' <= x <= 'z':
+        return ord(x) - ord('a') + 1
+    else:
+        return ord(x) - ord('A') + 27
+
+
+def solve():
+    backpacks = open("input.txt").readlines()
+
+    tlt1: int = 0
+    tlt2: int = 0
+    for backpack in backpacks:
+        backpack.strip()
+        b1, b2 = backpack[:len(backpack) // 2], backpack[len(backpack) // 2:]
+
+        for x in b1:
+            if x in b2:
+                tlt1 += calc_score(x)
+                break
+
+    for i in range(0, len(backpacks), 3):
+        for x in backpacks[i].strip():
+            if x in backpacks[i + 1] and x in backpacks[i + 2]:
+                tlt2 += calc_score(x)
+                break
+
+    print(f"Part 1: {tlt1}")
+    print(f"Part 2: {tlt2}")
+
+
+solve()
