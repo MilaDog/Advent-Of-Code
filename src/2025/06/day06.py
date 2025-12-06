fb4fe838c56e1cec698317f5f4771b85c681012c
MilaDog <daniel.ryan.sergeant@gmail.com>
2025-12-06 13:30:57 +0200

    Solution: 2025/06 [py]

---
+++ src/2025/06/day06.py
@@ -0,0 +1,91 @@
+from functools import reduce
+from pathlib import Path
+from timeit import timeit
+
+from src.timing import Timing
+
+
+class Solution:
+    """Solutions to the problems."""
+
+    def __init__(self, data: list[str]) -> None:
+        self.data: list[str] = data
+
+    @classmethod
+    def parse_input(cls) -> "Solution":
+        """Parse the problem data input to be used.
+
+        Returns:
+            Solution:
+                Class instance with the parsed input data.
+        """
+        with open(Path(__file__).resolve().parent / "input.txt", "r") as file:
+            values: list[str] = [line.strip("\n") for line in file.readlines()]
+
+        return cls(data=values)
+
+    def part_01(self) -> None:
+        """Solve Part 01 of the problem."""
+        tlt: int = 0
+
+        # want to split on all white spaces, then join with a whitespace, and split again.
+        # will remove multiple trailing whitespaces
+        # 345  43   5 -> 345 43 5 -> [345, 43, 5]
+        values: list[list[str]] = [" ".join(line.strip().split()).split() for line in self.data]
+
+        for vals in zip(*values):
+            match vals[-1]:
+                case "+":
+                    tlt += sum(map(int, vals[:-1]))
+
+                case "*":
+                    tlt += reduce(lambda acc, x: acc * x, map(int, vals[:-1]))
+
+                case _:
+                    assert f"Unknown match: {vals[-1]}"
+
+        print(f"Part 01: {tlt}")
+
+    def part_02(self) -> None:
+        """Solve Part 02 of the problem."""
+        tlt: int = 0
+
+        max_len: int = max(len(line) for line in self.data)
+
+        # padding to ensure all are same length
+        lines: list[str] = [line.ljust(max_len) for line in self.data]
+
+        # finding boundaries to split on
+        boundaries: list[int] = []
+        for i in range(max_len):
+            if all(line[i] == " " for line in lines):
+                if i > 0 and not all(line[i - 1] == " " for line in lines):
+                    boundaries.append(i)
+        boundaries.append(max_len)
+
+        # splitting on boundaries
+        values: list[list[str]] = []
+        ptr: int = -1
+        for boundary in boundaries:
+            values.append([line[ptr + 1 : boundary] for line in lines])
+            ptr = boundary
+
+        for cols in values:
+            match cols[-1].replace(" ", ""):
+                case "+":
+                    tlt += sum(map(int, ["".join(v) for v in zip(*cols[:-1])]))
+
+                case "*":
+                    tlt += reduce(lambda acc, x: acc * x, map(int, ["".join(v) for v in zip(*cols[:-1])]))
+
+                case _:
+                    assert f"Unknown match: {cols[-1]}"
+
+        print(f"Part 02: {tlt}")
+
+
+if __name__ == "__main__":
+    sol: Solution = Solution.parse_input()
+
+    print(Timing(timeit(sol.part_01, number=1)).result(), "\n")
+    print(Timing(timeit(sol.part_02, number=1)).result(), "\n")
--- src/2025/README.md
+++ src/2025/README.md
@@ -5,10 +5,17 @@
 - **Using**:
     - Python 3.13
 
-| Calendar                                      | Name                | Language(s) used | Stars         |
-|-----------------------------------------------|---------------------|------------------|---------------|
-| [Day 01](https://adventofcode.com/2025/day/1) | Secret Entrance     | Python           | :star: :star: |
-| [Day 02](https://adventofcode.com/2025/day/2) | Gift Shop           | Python           | :star: :star: |
-| [Day 03](https://adventofcode.com/2025/day/3) | Lobby               | Python           | :star: :star: |
-| [Day 04](https://adventofcode.com/2025/day/4) | Printing Department | Python           | :star: :star: |
-| [Day 05](https://adventofcode.com/2025/day/5) | Cafeteria           | Python           | :star: :star: |
+| Calendar                                       | Name                | Language(s) used | Stars         |
+|------------------------------------------------|---------------------|------------------|---------------|
+| [Day 01](https://adventofcode.com/2025/day/1)  | Secret Entrance     | Python           | :star: :star: |
+| [Day 02](https://adventofcode.com/2025/day/2)  | Gift Shop           | Python           | :star: :star: |
+| [Day 03](https://adventofcode.com/2025/day/3)  | Lobby               | Python           | :star: :star: |
+| [Day 04](https://adventofcode.com/2025/day/4)  | Printing Department | Python           | :star: :star: |
+| [Day 05](https://adventofcode.com/2025/day/5)  | Cafeteria           | Python           | :star: :star: |
+| [Day 06](https://adventofcode.com/2025/day/6)  | Trash Compactor     | Python           | :star: :star: |
+| [Day 07](https://adventofcode.com/2025/day/7)  | -                   | Python           | -             |
+| [Day 08](https://adventofcode.com/2025/day/8)  | -                   | Python           | -             |
+| [Day 09](https://adventofcode.com/2025/day/9)  | -                   | Python           | -             |
+| [Day 10](https://adventofcode.com/2025/day/10) | -                   | Python           | -             |
+| [Day 11](https://adventofcode.com/2025/day/11) | -                   | Python           | -             |
+| [Day 12](https://adventofcode.com/2025/day/12) | -                   | Python           | -             |
